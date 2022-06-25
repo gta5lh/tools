bf90970e18fb1c024fd5922c74782b414ab205f8
Maksim Logvinenko <lbyte00@gmail.com>
2022-06-25 11:15:45 +0300

    CU-2gg91w7 Implement static ID generator

---
+++ static_id_generator/go.mod
@@ -0,0 +1,5 @@
+module github.com/gta5lh/tools/static_id_generator
+
+go 1.18
+
+require github.com/juju/errors v0.0.0-20220622220526-54a94488269b
---
+++ static_id_generator/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/juju/errors"
+)
+
+const maxLen = 4
+
+// 36 characters
+var alphabet = []string{
+	"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+	"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+}
+
+func main() {
+	start := time.Now()
+	rand.Seed(time.Now().UnixNano())
+
+	count, err := generateStaticIDs(alphabet, maxLen)
+	if err != nil {
+		log.Fatal(errors.Annotate(err, "generating static IDs failed"))
+	}
+
+	fmt.Println(fmt.Sprintf("%v IDs generated in %v", count, time.Since(start)))
+}
+
+func generateStaticIDs(alphabet []string, maxLen int) (int, error) {
+	ids := generate(alphabet, maxLen)
+
+	f, err := os.Create("generated_static_ids.csv")
+	if err != nil {
+		return 0, errors.Annotate(err, "creating file failed")
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			fmt.Println(errors.Annotate(err, "closing file failed"))
+		}
+	}()
+
+	w := bufio.NewWriter(f)
+	for _, n := range ids {
+		if _, err := w.WriteString(fmt.Sprintf("%s\n", n)); err != nil {
+			return 0, errors.Annotate(err, "writing static ID failed")
+		}
+	}
+
+	if err := w.Flush(); err != nil {
+		return 0, errors.Annotate(err, "flushing failed")
+	}
+
+	return len(ids), nil
+}
+
+func generate(alphabet []string, maxLen int) (res []string) {
+	var ml []string
+	ml = alphabet
+	for _, symbol := range ml {
+		res = append(res, symbol)
+	}
+
+	for z := 0; z < maxLen-1; z++ {
+		var tmp []string
+
+		for i := 0; i < len(alphabet); i++ {
+			for k := 0; k < len(ml); k++ {
+				tmp = append(tmp, ml[k]+alphabet[i])
+			}
+		}
+
+		ml = tmp
+
+		rand.Shuffle(len(tmp), func(i, j int) {
+			tmp[i], tmp[j] = tmp[j], tmp[i]
+		})
+		res = append(res, tmp...)
+	}
+
+	return res
+}
